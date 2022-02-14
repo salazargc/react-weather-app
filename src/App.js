b25a1b7b0f2034de42f6c8334c722205e1956811
salazargc <74729190+salazargc@users.noreply.github.com>
2022-02-14 12:51:59 -0700

    added the Github repo url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Gabriela Salazar and is
+        <a
+          href="https://github.com/salazargc/react-weather-app"
+          rel="noreferrer"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
